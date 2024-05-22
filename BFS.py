8a7038cae63901ce4ba56e3e853a5c8d1666846c
Malakelis <awgeorgis@gmail.com>
2024-05-21 22:43:46 -0700

    bfs order

--- BFS.py
+++ BFS.py
@@ -1,17 +1,21 @@
 from collections import deque
 
 def bfs(graph, start_vertex):
+    visitedList = []
     distance = {vertex: float('inf') for vertex in graph}
     distance[start_vertex] = 0  # Distance from start vertex to itself is 0
 
-    queue = deque([start_vertex])
+    queue = deque(start_vertex)
+    visitedList.append(start_vertex)
     while queue:
-        print(queue)
         current_vertex = queue.popleft()
         for neighbor in graph[current_vertex]:
             if distance[neighbor] == float('inf'):
                 queue.append(neighbor)
+                if neighbor not in visitedList:
+                    visitedList.append(neighbor)
                 distance[neighbor] = distance[current_vertex] + 1
+    print(visitedList)
     return distance
 
 # Example graph represented as an adjacency list
